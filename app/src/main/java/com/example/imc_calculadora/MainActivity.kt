72b5291d773d0cc04348ca376c798b07126f5205
Fran <fmartosmuoz@yahoo.es>
2025-02-26 13:03:30 +0100

    añado mas cosas

--- app/src/main/java/com/example/imc_calculadora/MainActivity.kt
+++ app/src/main/java/com/example/imc_calculadora/MainActivity.kt
@@ -21,17 +21,21 @@ class MainActivity : AppCompatActivity() {
     lateinit var weightAddButton: Button
     lateinit var weightMinusButton: Button
 
+
     lateinit var calculateButton: Button
     lateinit var resultTextView: TextView
+    lateinit var descriptionTextView: TextView
 
     var height = 170.0f
     var weight = 75.0f
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -45,77 +49,61 @@ class MainActivity : AppCompatActivity() {
         weightAddButton = findViewById(R.id.weightAddButton)
         weightMinusButton = findViewById(R.id.weightMinusButton)
 
-
-
         calculateButton = findViewById(R.id.calculateButton)
         resultTextView = findViewById(R.id.resultTextView)
+        descriptionTextView = findViewById(R.id.descriptionTextView)
 
         heightSlider.addOnChangeListener { slider, value, fromUser ->
             height = value
             heightTextView.text = "${height.toInt()}"
-
         }
-        weightAddButton.setOnClickListener{
+
+        weightAddButton.setOnClickListener {
             weight ++
             weightTextView.text = "${weight.toInt()}"
         }
 
-        weightAddButton.setOnClickListener{
-
+        weightMinusButton.setOnClickListener {
             weight --
             weightTextView.text = "${weight.toInt()}"
         }
 
-
-        calculateButton.setOnClickListener{
-
-            val result = weight / (height/100).pow(2)
+        calculateButton.setOnClickListener {
+            val result = weight / (height / 100).pow(2)
 
             resultTextView.text = String.format(Locale.getDefault(), "%.2f", result)
 
-
-            var colorId =0
-
-
-            when(result){
-
-                in 0f..18.5f ->{
-
+            var colorId = 0
+            var textId = 0
+            when (result) {
+                in 0f..<18.5f -> {
+                    colorId = R.color.imc_underweight
+                    textId = R.string.imc_underweight
                 }
-                in 19f..40.0f ->{
-
+                in 18.5f..<25f -> {
+                    colorId = R.color.imc_normal
+                    textId = R.string.imc_normal
                 }
-                in 30f..45f ->{
-
+                in 25f..<30f -> {
+                    colorId = R.color.imc_overweight
+                    textId = R.string.imc_overweight
                 }
-                in 45f..80f ->{
-
+                in 30f..<35f -> {
+                    colorId = R.color.imc_obesity1
+                    textId = R.string.imc_obesity1
                 }
-
-                in 81..99 ->{
-
+                in 35f..<40f -> {
+                    colorId = R.color.imc_obesity2
+                    textId = R.string.imc_obesity2
+                }
+                else -> {
+                    colorId = R.color.imc_obesity3
+                    textId = R.string.imc_obesity3
                 }
             }
-
-
-
-
-
-
-
-
-
-                    color = R.color.imc_underweight
-                    color = R.color.imc_normal
-                    color = R.color.i
-                    color = R.color.imc_underweight
-                    color = R.color.imc_underweight
-                    color = R.color.imc_underweight
-
-
-
-            }
+            resultTextView.setTextColor(getColor(colorId))
+            descriptionTextView.setTextColor(getColor(colorId))
+            descriptionTextView.text = getString(textId)
         }
-
     }
 }
